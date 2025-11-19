d646412381c3d21bec730c9e2c3cf9ce3de669ef
lames4933-ui <lames4933@gmail.com>
2025-11-19 14:26:32 +0300

    lab2-arrays -part 1

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class Arrays {
+
+    public void Traversal(int[]a){
+        System.out.println("The elements are:");
+
+        for (int i=0;i<a.length;i++)
+            System.out.println("Index["+i+"]= "+a[i]);
+
+    }
+
+}
---
+++ src/Arrays/Main.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[]numbers={1,2,3,4,5};
+        int[]numbers2=new int[5];
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers2));
+
+
+
+    }
+
+}
