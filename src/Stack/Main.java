ce9e4ef7e2450bc37521704c4f1d9a23421a3797
lames4933-ui <lames4933@gmail.com>
2026-01-07 14:44:58 +0300

    Stack lab9

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -8,6 +8,7 @@ public class Main {
         aStack.push(10);
         aStack.push(20);
         aStack.push(30);
+        aStack.push(30);
         aStack.push(40);
         aStack.push(50);
         aStack.push(60);
